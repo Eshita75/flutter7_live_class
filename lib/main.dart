c97f8c1deca042bee92cb8f35ab58d3e8dd43979
Eshita75 <163004513+Eshita75@users.noreply.github.com>
2024-04-03 21:49:29 +0600

    successful

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,121 @@
-//Cupertino widget|| ThemeData
+// //Cupertino widget|| ThemeData
+//
+// import 'package:flutter/cupertino.dart';
+//
+// main(){
+//   runApp(IosStyleApp());
+// }
+//
+// class IosStyleApp extends StatelessWidget{
+//   const IosStyleApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return CupertinoApp(home: Home(),);
+//   }
+// }
+//
+// class Home extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     return CupertinoPageScaffold(
+//       navigationBar: CupertinoNavigationBar(
+//         middle: Text('Home'),
+//         trailing: CupertinoButton.filled(child: Text('hi'), onPressed: (){}),
+//       ),
+//         child:Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               CupertinoActivityIndicator(
+//                 radius: 30,
+//               ),
+//               CupertinoSwitch(value: false, onChanged: (value){})
+//             ],
+//           ),
+//         )
+//     );
+//   }
+// }
 
-import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
 
 main(){
-  runApp(IosStyleApp());
+  runApp(MyApp());
 }
 
-class IosStyleApp extends StatelessWidget{
-  const IosStyleApp({super.key});
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(home: Home(),);
+    return MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          // style: ElevatedButton.styleFrom(
+          //   backgroundColor: Colors.redAccent,
+          //   foregroundColor: Colors.black,
+          //   shape: RoundedRectangleBorder(
+          //     borderRadius: BorderRadius.circular(20)
+          //   )
+          //)
+            style: ButtonStyles.elevatedButton
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyles.textButton
+        )
+      ),
+    );
   }
 }
 
 class Home extends StatelessWidget{
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Home'),
-        trailing: CupertinoButton.filled(child: Text('hi'), onPressed: (){}),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
       ),
-        child:Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CupertinoActivityIndicator(
-                radius: 30,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){}, child: Text('Tap to Edit')),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple
               ),
-              CupertinoSwitch(value: false, onChanged: (value){})
-            ],
-          ),
-        )
+                onPressed: (){}, child: Text('Tap to Edit')),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.green
+              ),
+                onPressed: (){}, child: Text('Tap to Edit')),
+            TextField()
+          ],
+        ),
+      ),
     );
   }
+}
+
+class ButtonStyles{
+  static ButtonStyle elevatedButton = ElevatedButton.styleFrom(
+      backgroundColor: Colors.redAccent,
+      foregroundColor: Colors.black,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20)
+      )
+  );
+
+  static ButtonStyle textButton = TextButton.styleFrom(
+    textStyle: TextStyle(
+      fontSize: 24
+    ),
+    padding:  EdgeInsets.symmetric(vertical: 16, horizontal: 20)
+  );
 }
